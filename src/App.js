9e53228f6bf1e8ad6a03659ba24635e26b490202
Yogendra Sai <yogendrasai02@gmail.com>
2025-02-01 17:21:24 +0530

    added json-server to serve questions data via fake api, managing state using useReducer

--- src/App.js
+++ src/App.js
@@ -1,7 +1,40 @@
+import { useEffect, useReducer } from "react";
 import Header from "./Header";
 import Main from "./Main";
 
+const initialState = {
+  questions: [],
+  quizStatus: "LOADING",
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "DATA_RECEIVED":
+      return {
+        ...state,
+        questions: action.payload,
+        quizStatus: "READY",
+      };
+    default:
+      throw new Error("Unknown action type");
+  }
+};
+
 export default function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(() => {
+    fetch("http://localhost:8000/questions")
+      .then((res) => res.json())
+      .then((data) => {
+        dispatch({
+          type: "DATA_RECEIVED",
+          payload: data,
+        });
+      })
+      .catch((err) => console.error("Error fetching questions:", err));
+  }, []);
+
   return (
     <div className="app">
       <Header />
